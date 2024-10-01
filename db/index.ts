f283251ff578e2ed9650154f8cd4c4c7ffa59745
Murderlon <merlijn@soverin.net>
2024-10-01 16:48:01 +0200

    Remove unneeded deps

--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,11 @@
-import ws from 'ws'
 import { drizzle } from 'drizzle-orm/neon-serverless'
-import { Pool, neonConfig } from '@neondatabase/serverless'
-import { config } from 'dotenv'
-import { schema } from './schema'
+import { Pool } from '@neondatabase/serverless'
 import { Resource } from 'sst'
 
-config({ path: '.env' }) // or .env.local
+import { schema } from './schema'
 
-neonConfig.webSocketConstructor = ws
 const pool = new Pool({ connectionString: Resource.DATABASE_URL.value })
 
 export const db = drizzle(pool, { schema })
+
 export { schema } from './schema'
